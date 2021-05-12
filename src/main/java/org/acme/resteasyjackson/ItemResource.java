7092836e499c61f54311e2ba97d78197e1d5ed49
Felipe Yanaga <felipe@yanaga.com.br>
2021-05-12 09:44:43 -0400

    removed itemresource for heroku

--- src/main/java/org/acme/resteasyjackson/ItemResource.java
+++ src/main/java/org/acme/resteasyjackson/ItemResource.java
@@ -1,28 +1,28 @@
-package org.acme.resteasyjackson;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import java.util.stream.Stream;
-
-@Path("/items")
-@Produces("application/json")
-@Consumes("application/json")
-public class ItemResource {
-
-    @Inject
-    EntityManager entityManager;
-
-    @GET
-    public Item[] getItem(){
-        return entityManager.createNamedQuery("Items.findAll",Item.class).getResultList()
-                .toArray(new Item[0]);
-    }
-
-
-}
+//package org.acme.resteasyjackson;
+//
+//import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+//
+//import javax.inject.Inject;
+//import javax.persistence.EntityManager;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import java.util.stream.Stream;
+//
+//@Path("/items")
+//@Produces("application/json")
+//@Consumes("application/json")
+//public class ItemResource {
+//
+//    @Inject
+//    EntityManager entityManager;
+//
+//    @GET
+//    public Item[] getItem(){
+//        return entityManager.createNamedQuery("Items.findAll",Item.class).getResultList()
+//                .toArray(new Item[0]);
+//    }
+//
+//
+//}
